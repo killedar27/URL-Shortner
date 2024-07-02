f5ba5ddd620c422f3e51f26a871a0d94725dda0a
Prathmesh Killedar <90565798+killedar27@users.noreply.github.com>
2024-07-02 16:41:15 +0000

    resolve and shorten partially implemented

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,11 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/killedar27/URL-Shortner/helpers"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +19,31 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error{
+	body := new(request)
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse json"})
+	}
+
+	//implement rate limiting
+
+
+	// check if the input if an actual URL
+
+	if !govalidator.IsURL(body.URL){
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "its not actual URL"})
+	}
+
+	// check for domain error
+
+	if !helpers.RemoveDomainError(body.URL){
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "domain error"})
+	}
+
+	// enforce https, SSL
+	body.URL = helpers.EnforceHTTP(body.URL)
+
+	return nil
 }
